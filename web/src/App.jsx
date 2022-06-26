4e4b64782fbe919bd1a4dd651ec63a21ae6f8cc7
Mark Curtis-Williams <markcurtiswilliams@gmail.com>
2022-06-26 14:05:57 -0300

    Add table to display repos in App.jsx
    Add code to render and css to display repos in table format.

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -27,7 +27,33 @@ class App extends React.Component { // Create App class
   render() {
     if (this.state.dataIsLoaded) { // If the data has been loaded, display the data
       return (
-        // Display the data
+        <div class="Repos-table">
+          <div class="Repos-table-row">
+            <div class="header cell">Name</div>
+            <div class="header cell">Description</div>
+            <div class="header cell">Language</div>
+            <div class="header cell">Forks Count</div>
+          </div>
+          {
+            this.state.repos.map((repo, i) => {
+              let rowColor = '';
+              if ((i % 2) == 1) {
+                rowColor = "cell Odd";
+              }
+              else {
+                rowColor = "cell Even";
+              }
+              return (
+                <div class="Repos-table-row">
+                  <div class={rowColor}>{repo.name}</div>
+                  <div class={rowColor}>{repo.description}</div>
+                  <div class={rowColor}>{repo.language}</div>
+                  <div class={rowColor}>{repo.forks_count}</div>
+                </div>
+              );
+            })
+          }
+        </div>
       )
     }
     else { // If data has not been loaded, display loading message
