f26ac2fe8cd020b66ca2bf764a35c9fee2a948a5
Mark Curtis-Williams <markcurtiswilliams@gmail.com>
2022-06-26 16:42:49 -0300

    Make repos clickable and display README.md
    Make each repository clickable and bring up a display that shows the
    README.md file contents.

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 const fetch = require('node-fetch');
 
+
 import './App.css';
 
 class App extends React.Component { // Create App class
@@ -9,15 +10,20 @@ class App extends React.Component { // Create App class
 
     this.state = { // Create state for App
       repos: [], // Placeholder for loading in repos data
-      dataIsLoaded: false // Flag whether the data has been loaded
+      dataIsLoaded: false, // Flag whether the data has been loaded
+      language: [], // A list of all languages found in the repos data
+      languageFilter: null, // Filter the repos by the selected language (default: null)
+      displayRepo: false, // Boolean to determine whether a repo has been clicked and should be displayed
+      displayedRepo: {}, // The repo to be displayed
+      displayedREADME: null // The README.md file from the selected repo
     }
   }
 
   componentDidMount() { // When component mounts, read data from repos API running on localhost:4000
-    fetch ('http://localhost:4000/repos')
+    fetch ('http://localhost:4000/repos') // Fetch the repos from the API (B. 1.)
       .then((res) => res.json())
       .then((json) => {
-        json.sort(function (a, b) { // Sort json by reverse chronological order
+        json.sort(function (a, b) { // Sort json by reverse chronological order (B. 3.)
           if (a.created_at < b.created_at) {
             return 1;
           }
@@ -40,11 +46,30 @@ class App extends React.Component { // Create App class
         this.setState({ // Update the state to reflect that the data has been loaded
           repos: json,
           dataIsLoaded: true,
-          languages: languages,
-          languageFilter: null
+          languages: languages
 
         });
       });
+  }
+
+  displayRepo (repo) {
+    this.setState ({
+      displayRepo: true,
+      displayedRepo: repo
+    });
+    fetch ('https://raw.githubusercontent.com/'+repo.full_name+'/master/README.md')
+      .then((response) => {
+        if (response.ok) { return response.text();}
+      })
+      .then((result) => this.setState({ displayedREADME: result}));
+  }
+
+  hideRepo () {
+    this.setState ({
+      displayRepo: false,
+      displayedRepo: null,
+      displayedREADME: null
+    });
   }
 
   toggleFilter (language) {
@@ -61,50 +86,65 @@ class App extends React.Component { // Create App class
   }
   render() {
     if (this.state.dataIsLoaded) { // If the data has been loaded, display the data
-      return (
-        <div className="Repos-table">
-          <div className="Repos-table-row">
-            <div className="header cell">Name</div>
-            <div className="header cell">Description</div>
-            <div className="header cell">Language
-            {
-              this.state.languages.map(language => { // Create buttons for each language
-                let divClass = "languageButton";
-                if (language == this.state.languageFilter) {
-                  divClass +=  " selected";
-                }
-                return (<div className={divClass} onClick={this.toggleFilter.bind(this,language)} key={language}>{language}</div>)
-              })
-            }
+      if (this.state.displayRepo && this.state.displayedRepo) { // Display the repo data if one has been clicked (B. 5 - 8)
+        return (
+          <div className="Repo-display">
+            <div className="back-button" onClick={this.hideRepo.bind(this)}>Go back</div>
+            <div className="header repoCell">Name of repository:</div>
+            <div className="repoCell">{this.state.displayedRepo.name}</div>
+            <div className="header repoCell">Contents of README.md</div>
+            <div className="readme repoCell">
+              {(!!this.state.displayedREADME) ? this.state.displayedREADME : ""}
             </div>
-            <div className="header cell">Forks Count</div>
           </div>
-          {
-            this.state.repos.map((repo, i) => { // Display each repo in a row
-              if (!(this.state.languageFilter) || (repo.language == this.state.languageFilter)) { // Filter by language if languageFilter is present
-                let rowColor = '';
-                if ((i % 2) == 1) {
-                  rowColor = "cell Odd";
+        )
+      }
+      else { // Default repos table display
+        return (
+          <div className="Repos-table">
+            <div className="Repos-table-row">
+              <div className="header cell">Name</div>
+              <div className="header cell">Description</div>
+              <div className="header cell">Language
+              {
+                this.state.languages.map(language => { // Create buttons for each language (B. 4.)
+                  let divClass = "languageButton";
+                  if (language == this.state.languageFilter) {
+                    divClass +=  " selected";
+                  }
+                  return (<div className={divClass} onClick={this.toggleFilter.bind(this,language)} key={language}>{language}</div>)
+                })
+              }
+              </div>
+              <div className="header cell">Forks Count</div>
+            </div>
+            {
+              this.state.repos.map((repo, i) => { // Display each repo in a row (B. 2.)
+                if (!(this.state.languageFilter) || (repo.language == this.state.languageFilter)) { // Filter by language if languageFilter is present
+                  let rowColor = '';
+                  if ((i % 2) == 1) {
+                    rowColor = "cell Odd";
+                  }
+                  else {
+                    rowColor = "cell Even";
+                  }
+                  return (
+                    <div className="Repos-table-row" key={repo.id} onClick={this.displayRepo.bind(this, repo)}>
+                      <div className={rowColor}>{repo.name}</div>
+                      <div className={rowColor}>{repo.description}</div>
+                      <div className={rowColor}>{repo.language}</div>
+                      <div className={rowColor}>{repo.forks_count}</div>
+                    </div>
+                  );
                 }
                 else {
-                  rowColor = "cell Even";
+                  return;
                 }
-                return (
-                  <div className="Repos-table-row" key={repo.id}>
-                    <div className={rowColor}>{repo.name}</div>
-                    <div className={rowColor}>{repo.description}</div>
-                    <div className={rowColor}>{repo.language}</div>
-                    <div className={rowColor}>{repo.forks_count}</div>
-                  </div>
-                );
-              }
-              else {
-                return;
-              }
-            })
-          }
-        </div>
-      )
+              })
+            }
+          </div>
+        )
+      }
     }
     else { // If data has not been loaded, display loading message
       return (
