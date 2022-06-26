90c142378eb1448d48511026518864f83046bf94
Mark Curtis-Williams <markcurtiswilliams@gmail.com>
2022-06-26 14:51:08 -0300

    Add buttons to filter repos by language

--- web/src/App.jsx
+++ web/src/App.jsx
@@ -25,40 +25,82 @@ class App extends React.Component { // Create App class
             return -1;
           }
         });
+        let languages = []; // Temporary variable to hold lanuages found in the list of repos
+        json.map (repo => { // Add language to the list if it is not there already
+          let foundRepoLanguage = false;
+          languages.map (language => {
+            if (repo.language == language) {
+              foundRepoLanguage = true;
+            }
+          });
+          if (!foundRepoLanguage) {
+            languages.push(repo.language);
+          }
+        });
         this.setState({ // Update the state to reflect that the data has been loaded
           repos: json,
-          dataIsLoaded: true
+          dataIsLoaded: true,
+          languages: languages,
+          languageFilter: null
+
         });
       });
   }
 
+  toggleFilter (language) {
+    if (language == this.state.languageFilter) {
+      this.setState ({
+        languageFilter: null // Toggle the language filtering if the selected button is the existing languageFilter
+      });
+    }
+    else {
+      this.setState ({
+        languageFilter: language // Set the languageFilter in the state if no filterLanguage is present
+      });
+    }
+  }
   render() {
     if (this.state.dataIsLoaded) { // If the data has been loaded, display the data
       return (
-        <div class="Repos-table">
-          <div class="Repos-table-row">
-            <div class="header cell">Name</div>
-            <div class="header cell">Description</div>
-            <div class="header cell">Language</div>
-            <div class="header cell">Forks Count</div>
+        <div className="Repos-table">
+          <div className="Repos-table-row">
+            <div className="header cell">Name</div>
+            <div className="header cell">Description</div>
+            <div className="header cell">Language
+            {
+              this.state.languages.map(language => { // Create buttons for each language
+                let divClass = "languageButton";
+                if (language == this.state.languageFilter) {
+                  divClass +=  " selected";
+                }
+                return (<div className={divClass} onClick={this.toggleFilter.bind(this,language)} key={language}>{language}</div>)
+              })
+            }
+            </div>
+            <div className="header cell">Forks Count</div>
           </div>
           {
-            this.state.repos.map((repo, i) => {
-              let rowColor = '';
-              if ((i % 2) == 1) {
-                rowColor = "cell Odd";
+            this.state.repos.map((repo, i) => { // Display each repo in a row
+              if (!(this.state.languageFilter) || (repo.language == this.state.languageFilter)) { // Filter by language if languageFilter is present
+                let rowColor = '';
+                if ((i % 2) == 1) {
+                  rowColor = "cell Odd";
+                }
+                else {
+                  rowColor = "cell Even";
+                }
+                return (
+                  <div className="Repos-table-row" key={repo.id}>
+                    <div className={rowColor}>{repo.name}</div>
+                    <div className={rowColor}>{repo.description}</div>
+                    <div className={rowColor}>{repo.language}</div>
+                    <div className={rowColor}>{repo.forks_count}</div>
+                  </div>
+                );
               }
               else {
-                rowColor = "cell Even";
+                return;
               }
-              return (
-                <div class="Repos-table-row">
-                  <div class={rowColor}>{repo.name}</div>
-                  <div class={rowColor}>{repo.description}</div>
-                  <div class={rowColor}>{repo.language}</div>
-                  <div class={rowColor}>{repo.forks_count}</div>
-                </div>
-              );
             })
           }
         </div>
